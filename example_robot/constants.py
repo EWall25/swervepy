30f7ff01a3f6b69b2e64e459c18954d3ded7cb7e
Jon Solera <otherjon@emufarm.org>
2023-12-25 14:38:29 -0800

    Refactor example robot so that new users can get a test working by only editing values in constants.py

---
+++ example_robot/constants.py
@@ -0,0 +1,125 @@
+"""
+This file defines constants related to your robot.  These constants include:
+
+ * Physical constants (exterior dimensions, wheel base)
+
+ * Mechanical constants (gear reduction ratios)
+
+ * Electrical constants (current limits, CAN bus IDs, roboRIO slot numbers)
+
+ * Operation constants (desired max velocity, max turning speed)
+
+ * Software constants (USB ID for driver joystick)
+"""
+
+import math
+from collections import namedtuple
+import rev
+from swervepy import u
+
+# Physical constants
+phys_data = {
+    "track_width": (21.73 * u.inch).m_as(u.m),
+    "wheel_base": (21.73 * u.inch).m_as(u.m),
+    "wheel_circumference": 4 * math.pi * u.inch,
+}
+PHYS = namedtuple("Data", phys_data.keys())(**phys_data)
+
+# Mechanical constants
+mech_data = {
+    "swerve_module_propulsion_gearing_ratio": 6.75,   # SDS Mk4i L2
+    "swerve_module_steering_gearing_ratio": 150 / 7,  # SDS Mk4i
+
+    "propulsion_motor_inverted": False,
+    "steering_motor_inverted": False,
+}
+MECH = namedtuple("Data", mech_data.keys())(**mech_data)
+
+# Electrical constants
+elec_data = {
+    # These current limit parameters are per-motor in the swerve modules
+    "continuous_current_limit": 40,
+    "peak_current_limit": 60,
+
+    # Talon FX motor controllers can set peak_current_duration.
+    # SparkMAX motor controllers can't.
+    #"peak_current_duration": 0.01,
+
+    # time in seconds for propulsion motors to ramp up to full speed
+    # reference: https://codedocs.revrobotics.com/java/com/revrobotics/cansparkmax
+    "open_loop_ramp_rate": 0.5,
+    "closed_loop_ramp_rate": 0.5,
+
+    "RF_steer_CAN_ID": 11,
+    "RF_drive_CAN_ID": 12,
+    "RF_encoder_DIO": 0,
+    "RB_steer_CAN_ID": 13,
+    "RB_drive_CAN_ID": 14,
+    "RB_encoder_DIO": 1,
+    "LB_steer_CAN_ID": 15,
+    "LB_drive_CAN_ID": 16,
+    "LB_encoder_DIO": 2,
+    "LF_steer_CAN_ID": 17,
+    "LF_drive_CAN_ID": 18,
+    "LF_encoder_DIO": 3,
+}
+ELEC = namedtuple("Data", elec_data.keys())(**elec_data)
+
+JOYSTICK_AXES = {
+  "LEFT_X": 0,
+  "LEFT_Y": 1,
+  "RIGHT_X": 4,
+  "RIGHT_Y": 5,
+}
+
+# Operation constants
+op_data = {
+    # These maximum parameters reflect the maximum physically possible, not the
+    # desired maximum limit.
+    "max_speed": 4.5 * (u.m / u.s),
+    "max_angular_velocity": 11.5 * (u.rad / u.s),
+
+    # For NEO / SparkMAX, use the following and comment out the Falcon500 values
+    #"propulsion_neutral": rev.CANSparkMax.IdleMode.kCoast,
+    #"steering_neutral": rev.CANSparkMax.IdleMode.kBrake,
+    # For Falcon500 / TalonFX, use the following and comment out the NEO values
+    "propulsion_neutral": phoenix5.NeutralMode.Coast,
+    "steering_neutral": phoenix5.NeutralMode.Brake,
+
+    # Values to pass to stick.getRawAxis()
+    # Set these according to your operator preferences
+    "translation_joystick_axis": JOYSTICK_AXES["RIGHT_Y"],
+    "strafe_joystick_axis": JOYSTICK_AXES["RIGHT_X"],
+    "rotation_joystick_axis": JOYSTICK_AXES["LEFT_X"],
+}
+OP = namedtuple("Data", op_data.keys())(**op_data)
+
+# Software constants
+sw_data = {
+    # field_relative: True if "forward" means "down the field"; False if
+    # "forward" means "in the direction the robot is facing".  A True value
+    # requires a (non-Dummy) gyro.
+    "field_relative": False,
+
+    # open_loop: True if we're not using PID control *for velocity targeting*,
+    # i.e. when a target velocity is calculated, do we use the corresponding
+    # CoaxialDriveComponent's follow_velocity_open() method (set motor output
+    # proportionally based on target and max velocities) or
+    # follow_velocity_closed() method (put motor in PID control mode and set
+    # target velocity).
+    #
+    "open_loop": True,
+
+    # Constants for PID control of the propulsion AND steering motors
+    # (kP must be non-zero, or azimuth motors won't engage.)
+    "kP": 0.3,   # representative value for Falcon500 motors
+    #"kP": 0.01,   # representative value for NEO motors
+    "kI": 0,
+    "kD": 0,
+
+    # Constants for feed-forward of propulsion motors
+    "kS": 0,
+    "kV": 0,
+    "kA": 0,
+}
+SW = namedtuple("Data", sw_data.keys())(**sw_data)
--- example_robot/container.py
+++ example_robot/container.py
@@ -1,115 +1,146 @@
 import math
+import logging
+logger = logging.getLogger("your.robot")
 
-import phoenix5
 import wpilib
 from wpimath.geometry import Translation2d, Rotation2d, Pose2d
 from pathplannerlib.path import PathPlannerPath, PathConstraints, GoalEndState
 
 from swervepy import u, SwerveDrive, TrajectoryFollowerParameters
-from swervepy.impl import (
-    PigeonGyro,
-    CoaxialSwerveModule,
-    Falcon500CoaxialDriveComponent,
-    Falcon500CoaxialAzimuthComponent,
-    AbsoluteCANCoder,
-)
+from swervepy.impl import CoaxialSwerveModule
 
+from constants import PHYS, MECH, ELEC, OP, SW
+import components
 
 class RobotContainer:
+    """
+    This example robot container should serve as a demonstration for how to
+    implement swervepy on your robot.  You should not need to edit much of the
+    code in this module to get a test working.  Instead, edit the values and
+    class choices in constants.py.
+    """
     def __init__(self):
-        field_relative = True
-        open_loop = True
+        gyro = components.gyro_component_class(**components.gyro_param_values)
 
-        track_width = (21.73 * u.inch).m_as(u.m)
-        wheel_base = (21.73 * u.inch).m_as(u.m)
-        max_speed = 4.5 * (u.m / u.s)
-        max_angular_velocity = 11.5 * (u.rad / u.s)
-
-        # Each component defines a Parameters dataclass with any options applicable to all instances of that component.
-        # For example, wheel circumference is the same for all four modules on a SDS Mk4 drive base, so that is included
-        # in the Parameters class. Motor IDs are different for each instance of a module, so those are not included.
-        drive_params = Falcon500CoaxialDriveComponent.Parameters(
-            wheel_circumference=4 * math.pi * u.inch,
-            gear_ratio=6.75 / 1,  # SDS Mk4i L2
-            max_speed=max_speed,
-            open_loop_ramp_rate=0,
-            closed_loop_ramp_rate=0,
-            continuous_current_limit=40,
-            peak_current_limit=60,
-            peak_current_duration=0.01,
-            neutral_mode=phoenix5.NeutralMode.Coast,
-            kP=0,
-            kI=0,
-            kD=0,
-            kS=0,
-            kV=0,
-            kA=0,
-            invert_motor=False,
-        )
-        azimuth_params = Falcon500CoaxialAzimuthComponent.Parameters(
-            gear_ratio=150 / 7,  # SDS Mk4i
-            max_angular_velocity=max_angular_velocity,
-            ramp_rate=0,
-            continuous_current_limit=40,
-            peak_current_limit=60,
-            peak_current_duration=0.01,
-            neutral_mode=phoenix5.NeutralMode.Brake,
-            kP=0,
-            kI=0,
-            kD=0,
-            invert_motor=False,
-        )
-
-        gyro = PigeonGyro(0, True)
-
-        # When defining module positions for kinematics, +x values represent moving toward the front of the robot, and
-        # +y values represent moving toward the left of the robot
+        # The Azimuth component included the absolute encoder because it needs
+        # to be able to reset to absolute position.
+        #
+        self.lf_enc = components.absolute_encoder_class(ELEC.LF_encoder_DIO)
+        self.lb_enc = components.absolute_encoder_class(ELEC.LB_encoder_DIO)
+        self.rb_enc = components.absolute_encoder_class(ELEC.RB_encoder_DIO)
+        self.rf_enc = components.absolute_encoder_class(ELEC.RF_encoder_DIO)
         modules = (
-            # Swerve module implementations are as general as possible (coaxial, differential) but take specific components
-            # like Falcon or NEO drive motors as arguments.
+            # Left Front module
             CoaxialSwerveModule(
-                # Pass in general Parameters and module-specific options
-                Falcon500CoaxialDriveComponent(0, drive_params),
-                # The Azimuth component included the CANCOder (absolute encoder) because it needs to be able to
-                # reset to absolute position
-                Falcon500CoaxialAzimuthComponent(4, Rotation2d.fromDegrees(0), azimuth_params, AbsoluteCANCoder(0)),
-                Translation2d(wheel_base / 2, track_width / 2),
+                drive=components.drive_component_class(
+                    id_=ELEC.LF_drive_CAN_ID,
+                    parameters=components.drive_params),
+                azimuth=components.azimuth_component_class(
+                    id_=ELEC.LF_steer_CAN_ID,
+                    azimuth_offset=Rotation2d.fromDegrees(0),
+                    parameters=components.azimuth_params,
+                    absolute_encoder=self.lf_enc),
+                placement=Translation2d(*components.module_locations['LF']),
             ),
+            # Right Front module
             CoaxialSwerveModule(
-                Falcon500CoaxialDriveComponent(1, drive_params),
-                Falcon500CoaxialAzimuthComponent(5, Rotation2d.fromDegrees(0), azimuth_params, AbsoluteCANCoder(1)),
-                Translation2d(wheel_base / 2, -track_width / 2),
+                drive=components.drive_component_class(
+                    id_=ELEC.RF_drive_CAN_ID,
+                    parameters=components.drive_params),
+                azimuth=components.azimuth_component_class(
+                    id_=ELEC.RF_steer_CAN_ID,
+                    azimuth_offset=Rotation2d.fromDegrees(0),
+                    parameters=components.azimuth_params,
+                    absolute_encoder=self.rf_enc),
+                placement=Translation2d(*components.module_locations['RF']),
             ),
+            # Left Back module
             CoaxialSwerveModule(
-                Falcon500CoaxialDriveComponent(2, drive_params),
-                Falcon500CoaxialAzimuthComponent(6, Rotation2d.fromDegrees(0), azimuth_params, AbsoluteCANCoder(2)),
-                Translation2d(-wheel_base / 2, track_width / 2),
+                drive=components.drive_component_class(
+                    id_=ELEC.LB_drive_CAN_ID,
+                    parameters=components.drive_params),
+                azimuth=components.azimuth_component_class(
+                    id_=ELEC.LB_steer_CAN_ID,
+                    azimuth_offset=Rotation2d.fromDegrees(0),
+                    parameters=components.azimuth_params,
+                    absolute_encoder=self.lb_enc),
+                placement=Translation2d(*components.module_locations['LB']),
             ),
+            # Right Back module
             CoaxialSwerveModule(
-                Falcon500CoaxialDriveComponent(3, drive_params),
-                Falcon500CoaxialAzimuthComponent(7, Rotation2d.fromDegrees(0), azimuth_params, AbsoluteCANCoder(3)),
-                Translation2d(-wheel_base / 2, -track_width / 2),
+                drive=components.drive_component_class(
+                    id_=ELEC.RB_drive_CAN_ID,
+                    parameters=components.drive_params),
+                azimuth=components.azimuth_component_class(
+                    id_=ELEC.RB_steer_CAN_ID,
+                    azimuth_offset=Rotation2d.fromDegrees(0),
+                    parameters=components.azimuth_params,
+                    absolute_encoder=self.rb_enc),
+                placement=Translation2d(*components.module_locations['RB']),
             ),
         )
 
         self.stick = wpilib.Joystick(0)
 
-        # Define a swerve drive subsystem by passing in a list of SwerveModules and some options
-        self.swerve = SwerveDrive(modules, gyro, max_speed, max_angular_velocity)
+        # Define a swerve drive subsystem by passing in a list of SwerveModules
+        # and some options
+        #
+        self.swerve = SwerveDrive(
+            modules, gyro, OP.max_speed, OP.max_angular_velocity)
 
+        # Set the swerve subsystem's default command to teleoperate using
+        # the controller joysticks
+        #
         self.swerve.setDefaultCommand(
             self.swerve.teleop_command(
-                lambda: deadband(-self.stick.getRawAxis(1), 0.05),
-                lambda: deadband(-self.stick.getRawAxis(0), 0.05),
-                lambda: deadband(-self.stick.getRawAxis(4), 0.1),  # Invert for CCW+
-                field_relative,
-                open_loop,
+                translation=self.get_translation_input,
+                strafe=self.get_strafe_input,
+                rotation=self.get_rotation_input,
+                field_relative=SW.field_relative,
+                open_loop=SW.open_loop,
             )
         )
 
+    def log_data(self):
+        for pos in ("LF", "RF", "LB", "RB"):
+            encoder = getattr(self, f"{pos.lower()}_enc")
+            wpilib.SmartDashboard.putNumber(
+                f"{pos} absolute encoder", encoder.absolute_position_degrees)
+            wpilib.SmartDashboard.putNumber(
+                f"{pos} absolute encoder", encoder.absolute_position_degrees)
+
+    @staticmethod
+    def deadband(value, band):
+        return value if abs(value) > band else 0
+
+    def process_joystick_input(self, val, deadband=0.1, exponent=1,
+                               invert=False):
+        """
+        Given a raw joystick reading, return the processed value after adjusting
+        for real-world UX considerations:
+          * apply a deadband to ignore jitter around zero
+          * apply an exponent for greater low-velocity control
+        """
+        deadbanded_input = self.deadband(val, deadband)
+        input_sign = +1 if val > 0 else -1   # this works for val=0 also
+        invert_sign = -1 if invert else +1
+        # abs required for fractional exponents
+        scaled_input = abs(deadbanded_input)**exponent
+        return invert_sign * input_sign * scaled_input
+
+    def get_translation_input(self, invert=True):
+        raw_stick_val = self.stick.getRawAxis(OP.translation_joystick_axis)
+        return sign * self.process_joystick_input(raw_stick_val, invert=invert)
+
+    def get_strafe_input(self, invert=True):
+        raw_stick_val = self.stick.getRawAxis(OP.strafe_joystick_axis)
+        return sign * self.process_joystick_input(raw_stick_val, invert=invert)
+
+    def get_rotation_input(self, invert=True):
+        raw_stick_val = self.stick.getRawAxis(OP.rotation_joystick_axis)
+        return sign * self.process_joystick_input(raw_stick_val, invert=invert)
+
     def get_autonomous_command(self):
-        # Example path-following autonomous routine
-        # Doesn't work yet for 2024 because RobotPy is missing some commands
         follower_params = TrajectoryFollowerParameters(
             max_drive_velocity=4.5 * (u.m / u.s),
             theta_kP=1,
@@ -121,15 +152,12 @@ class RobotContainer:
             Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
             Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90)),
         ])
-
         path = PathPlannerPath(
             bezier_points,
             PathConstraints(3.0, 3.0, 2 * math.pi, 4 * math.pi),
             GoalEndState(0.0, Rotation2d.fromDegrees(-90)),     # Zero velocity and facing 90 degrees clockwise
         )
 
-        return self.swerve.follow_trajectory_command(path, follower_params, True, True)
-
-
-def deadband(value, band):
-    return value if abs(value) > band else 0
+        first_path = True  # reset robot pose to initial pose in trajectory
+        open_loop = True   # don't use built-in motor feedback for velocity
+        return self.swerve.follow_trajectory_command(path, follower_params, first_path, open_loop)
