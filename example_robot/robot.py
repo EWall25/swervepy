e5501a4f6f2510008095d7302d6be185364b69d2
Ethan Wall <ewall25@jesuittampa.org>
2024-01-11 00:45:50 -0500

    Compat with new RobotPy deploy system in example project

--- example_robot/robot.py
+++ example_robot/robot.py
@@ -1,10 +1,10 @@
-import sys
-from pathlib import Path
+# You must either:
+#   1) Hardlink swervepy into the example_robot directory OR
+#   2) Move the example code to a different project and copy swervepy into that project
+
 from typing import Optional
 
 import commands2
-
-sys.path.append(str(Path(__file__).parent.parent.absolute()))
 
 from container import RobotContainer
 
